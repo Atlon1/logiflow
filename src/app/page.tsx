94d374dd8af0a7c1fa6d0cc93df16229392d14ea
Atlon1 <maciej.szajstek@gmail.com>
2024-02-14 20:22:16 +0100

    add component to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import {Header} from './components/header';
+import {Hero} from "@/app/components/hero";
+import {Cars} from "@/app/components/cars";
+import {About} from "@/app/components/about";
+import {Why} from "@/app/components/why";
+import {Testimonial} from "@/app/components/testimonial";
+import {Cta} from "@/app/components/cta";
+import {Footer} from "@/app/components/footer";
+import {BackToTop} from "@/app/components/back-to-top";
+
 
 export default function Home() {
-  return (
-    <main className="">
-      Main
-    </main>
-  );
+    return (
+        <main className="">
+            <Header/>
+            <Hero/>
+            <Cars/>
+            <About/>
+            <Why/>
+            <Testimonial/>
+            <Cta/>
+            <Footer/>
+            <BackToTop/>
+        </main>
+    );
 }
