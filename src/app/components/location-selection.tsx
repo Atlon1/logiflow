0e33f44b5e07adea4dcad4fe0469517c916550be
Atlon1 <maciej.szajstek@gmail.com>
2024-02-21 20:52:41 +0100

    added

--- src/app/components/location-selection.tsx
+++ src/app/components/location-selection.tsx
@@ -19,8 +19,10 @@ export const LocationSelection = () => {
     return (
         <Menu as="div" className="w-full h-full flex xl:flex-row">
             <div className='relative flex-1'>
-                <Menu.Button className='dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8'>
-                    <div className='w-full h-16 xl:h-full flex justify-center xl:justify-start xl:border-r xl:border-black/10'>
+                <Menu.Button
+                    className='dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8'>
+                    <div
+                        className='w-full h-16 xl:h-full flex justify-center xl:justify-start xl:border-r xl:border-black/10'>
                         <div className='flex flex-col justify-center'>
                             <div className='flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0'>
                                 <FaMapMarkerAlt className='text-accent'/>
@@ -28,7 +30,8 @@ export const LocationSelection = () => {
                                     Select location
                                 </div>
                             </div>
-                            <div className='uppercase font-medium text-[13px] text-secondary text-center xl:ml-6 xl:text-left'>
+                            <div
+                                className='uppercase font-medium text-[13px] text-secondary text-center xl:ml-6 xl:text-left'>
                                 {location}
                             </div>
                         </div>
