c857143c93279537e6a19de1f36a7c86f9f48688
Atlon1 <maciej.szajstek@gmail.com>
2024-02-22 22:04:03 +0100

    addeed date selection on desktop

--- src/app/components/date-selection.tsx
+++ src/app/components/date-selection.tsx
@@ -10,6 +10,9 @@ import {FaArrowRightLong} from "react-icons/fa6";
 import {DateRange} from "react-date-range";
 import {format, addDays} from "date-fns";
 
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+
 const locations = [
     'Main Street 123, United States',
     'Business Avenue 456, Canada',
@@ -18,38 +21,36 @@ const locations = [
 
 export const DateSelection = () => {
 
-    const [location, setLocation] = useState('Select Location');
+    const [date, setDate] = useState([{
+        startDate: new Date(),
+        endDate: null,
+        key: 'selection'
+    }
+    ])
+
 
     return (
         <Menu as="div" className="w-full h-full flex xl:flex-row">
             <div className='relative flex-1'>
                 <Menu.Button
                     className='dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8'>
-                    <div
-                        className='w-full h-16 xl:h-full flex justify-center xl:justify-start xl:border-r xl:border-black/10'>
-                        <div className='flex flex-col justify-center'>
-                            <div className='flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0'>
-                                <FaCalendarAlt className='text-accent'/>
-                                <div className='text-[15px] uppercase font-bold'>
-                                    Select location
-                                </div>
-                            </div>
-                            <div
-                                className='uppercase font-medium text-[13px] text-secondary text-center xl:ml-6 xl:text-left'>
-                                {location}
-                            </div>
+                    <div className='flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0'>
+                        <FaCalendarAlt className='text-accent'/>
+                        <div className='text-[15px] uppercase font-bold'>
+                            Select Date
                         </div>
                     </div>
                 </Menu.Button>
-                <Menu.Items className='dropdown-menu shadow-lg absolute -top-56 xl:top-[90px] left-1/2 xl:left-0 z-10 transform
-                -translate-x-1/2 xl:-translate-x-0 text-sm text-center xl:text-left w-full bg-white max-w-[332px] py-6
-                rounded-[10px]'>
-                    {locations.map((location, index) => {
-                        return <div
-                            onClick={() => setLocation(location)}
-                            className='cursor-pointer py-4 xl:pl-10 hover:bg-gray-50 text-[13px] uppercase'
-                            key={index}>{location}</div>
-                    })}
+                <Menu.Items className='dropdown-menu shadow-lg absolute -top-56 xl:top-[90px] left-1/2 xl:left-0 z-50 transform
+                -translate-x-1/2 xl:-translate-x-0 rounded-[10px] overflow-hidden'>
+                    <DateRange
+                        onChange={(item : any) => setDate([item.selection])}
+                        editableDateInputs={true}
+                        moveRangeOnFirstSelection={false}
+                        ranges={date}
+                        rangeColors={['#ed1d24']}
+                        minDate={addDays(new Date(), 0)}
+                    />
                 </Menu.Items>
             </div>
         </Menu>
