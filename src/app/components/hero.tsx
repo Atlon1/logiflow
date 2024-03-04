e0356236b868ad627d25e87978fe367cae3352fd
Atlon1 <maciej.szajstek@gmail.com>
2024-03-04 23:00:01 +0100

    added serach

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -14,7 +14,10 @@ export const Hero = () => {
     return (
         <section className="h-screen xl:h-[90vh] bg-[#b2b7c2]/10" id='home'>
             <div className='container mx-auto h-full xl:pt-10'>
-
+                <div>
+                    <div>text</div>
+                    <div>image</div>
+                </div>
             </div>
             {searchActive ? (
                 <div className='fixed top-[80px] z-10 w-full max-w-[1920px]'>
--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -12,10 +12,13 @@ export const Search = () => {
     return (
         <div
             className={`${searchActive ? 'bg-white rounded-none xl:h-[80px]' : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'} hidden xl:block w-full relative shadow-lg`}>
-            <div className='xl:h-full flex items-center px-6 xl:px-0'>
+            <div className={`flex h-full ${searchActive ? 'justify-between' : 'justify-center'}`}>
                 <LocationSelection/>
                 <DateSelection/>
                 <HoursSelection/>
+                <div className='xl:h-full flex items-center px-6 xl:px-0'>
+                    <button className={`${searchActive ? 'btn btn-sm btn-accent w-[164px]' : 'btn btn-sm btn-accent xl:w-[184px]'}`}>Serach</button>
+                </div>
             </div>
         </div>
     )
