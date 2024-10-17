b175fc2146f7e8ed1fd886eed4a05fa4af2d7cbd
Atlon1 <maciej.szajstek@gmail.com>
2024-10-17 21:37:20 +0200

    about us complete

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -20,7 +20,12 @@ export const About = () => {
         >
             <div className='container mx-auto'>
                 <div className='flex flex-col xl:flex-row xl:justify-between'>
-                    <div>
+                    <motion.div
+                        variants={fadeIn('up',0.2)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{once: false, amount: 0.6}}
+                        className='flex-1 mb-8 xl:mb-0'>
                         <Image
                             className='rounded-[20px]'
                             src={'/images/about/car01.png'}
@@ -28,9 +33,74 @@ export const About = () => {
                             width={600}
                             height={448}
                         />
+                    </motion.div>
+                    <div className='flex-1 flex items-center xl:justify-center'>
+                        <div className='xl:max-w-[517px]'>
+                            <motion.h2
+                                variants={fadeIn('up',0.4)}
+                                initial='hidden'
+                                whileInView={'show'}
+                                viewport={{once: false, amount: 0.2}}
+                                className='h2'>
+                                Car services simplifiad.
+                            </motion.h2>
+                            <motion.p
+                                variants={fadeIn('up',0.6)}
+                                initial='hidden'
+                                whileInView={'show'}
+                                viewport={{once: false, amount: 0.2}}
+                                className='mb-[42px] max-w-md'>
+                                Rent, choose, and repair with ease. Our convenient locations, diverse car types, and
+                                reliable repair ensure a seamless car experience.
+                            </motion.p>
+                            <motion.div
+                                variants={fadeIn('up',0.8)}
+                                initial='hidden'
+                                whileInView={'show'}
+                                viewport={{once: false, amount: 0.2}}
+                                className='flex items-center gap-x-8 mb-12'>
+                                <div className='flex flex-col w-[100px]'>
+                                    <MdOutlineDirectionsCar className='text-5xl text-accent'/>
+                                    <div className='text-3xl font-black mb-2'>
+                                        {inView ? <CountUp start={0} end={50} duration={3} delay={3}/> : null}
+                                        +
+                                    </div>
+                                    <div className='uppercase text-[13px] font-semibold text-secondary'>
+                                        Car <br/> Types
+                                    </div>
+                                </div>
+                                <div className='flex flex-col w-[100px]'>
+                                    <MdOutlineMapsHomeWork className='text-5xl text-accent'/>
+                                    <div className='text-3xl font-black mb-2'>
+                                        {inView ? <CountUp start={0} end={135} duration={3} delay={3}/> : null}
+                                        +
+                                    </div>
+                                    <div className='uppercase text-[13px] font-semibold text-secondary'>
+                                        rental <br/> outlets
+                                    </div>
+                                </div>
+                                <div className='flex flex-col w-[100px]'>
+                                    <MdOutlineBuildCircle className='text-5xl text-accent'/>
+                                    <div className='text-3xl font-black mb-2'>
+                                        {inView ? <CountUp start={0} end={35} duration={3} delay={3}/> : null}
+                                        +
+                                    </div>
+                                    <div className='uppercase text-[13px] font-semibold text-secondary'>
+                                        repair <br/> points
+                                    </div>
+                                </div>
+                            </motion.div>
+                            <motion.button
+                                variants={fadeIn('up',1)}
+                                initial='hidden'
+                                whileInView={'show'}
+                                viewport={{once: false, amount: 0.2}}
+                                className='hidden xl:block bg-accent hover:bg-accent-hover rounded-[10px] w-full h-16 uppercase font-medium text-white tracking-[2px] text-[13px] max-w-[184px]'>See
+                                all cars
+                            </motion.button>
+                        </div>
                     </div>
 
-                    <div>text & stats</div>
                 </div>
             </div>
         </section>
